0ee1a8c8b1b232037758c6461b66f40c5b77f2ec
Nikkitha Ramesh <nikkitha.nikki96@gmail.com>
2025-12-11 10:54:40 +0100

    adding comments

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
 import os
 from utils import tsv_to_parquet, inspect_parquet_folder
 from analysis import final_analysis
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="PySpark local job")
+    
+    parser.add_argument(
+         "-i", "--input",
+        required=True,
+        help="Path to input tsv file "
+    )
+
+    parser.add_argument(
+        "-o", "--output",
+        required=True,
+        default=os.getcwd(),
+        help="Path to output folder "
+    )
+
+    return parser.parse_args()
 
 
 def main():
@@ -8,52 +28,44 @@ def main():
     Main ETL + analysis pipeline for LastFM dataset.
     """
 
-    # ---------------------------------------------------------
-    # 1. Define input and output paths
-    # ---------------------------------------------------------
-    tsv_input_path = os.getenv("TSV_INPUT_PATH")
-    parquet_output_path = os.getenv("PARQUET_OUTPUT_PATH", "/app/output_data/parquet")
-    tsv_final_output = os.getenv("FINAL_TSV_OUTPUT", "/app/output_data/results")
+   #Initialise parser and define path arguments
+    
+    args = parse_args()
 
+    tsv_input_path = args.tsv_input_path
+    parquet_path = os.makedirs(os.path.join(tsv_input_path, "parquet_path"))    
+    output_path = args.output_path
     if not tsv_input_path:
         raise ValueError(
             "ERROR: Input file not found"
         )
 
-    print(f"\n=== Running with configuration ===")
-    print(f"Input TSV:       {tsv_input_path}")
-    print(f"Parquet Output:  {parquet_output_path}")
-    print(f"Final TSV Output:{tsv_final_output}\n")
+    print(f"\n Running Analysis with the following user provided parameters")
+    print(f"Input TSV: {tsv_input_path}")
 
-    # ---------------------------------------------------------
-    # 2. Convert TSV -> Parquet
-    # ---------------------------------------------------------
-    tsv_to_parquet(tsv_input_path, parquet_output_path)
+    print(f"Final CSV Output: {output_path}\n")
 
-    # ---------------------------------------------------------
-    # 3. Load parquet dataset
-    # ---------------------------------------------------------
-    df, row_count, col_count = inspect_parquet_folder(parquet_output_path)
+    #Convert TSV -> Parquet
+    tsv_to_parquet(tsv_input_path, parquet_path)
 
-    # ---------------------------------------------------------
-    # 4. Run final analysis
-    # ---------------------------------------------------------
-    print("\n===== Top 10 Songs (Final Analysis) =====")
+    #Load parquet dataset
+    df = inspect_parquet_folder(parquet_path)
+
+    #Run final analysis
+    print("\n Top 10 Songs:")
     result_df = final_analysis(df, show_output=True)
 
-    # ---------------------------------------------------------
-    # 5. Save results as TSV
-    # ---------------------------------------------------------
+    #Save results as TSV
     (
         result_df.coalesce(1)
         .write
         .mode("overwrite")
         .option("delimiter", "\t")
         .option("header", "true")
-        .csv(tsv_final_output)
+        .csv(output_path)
     )
 
-    print(f"\n Final results written to: {tsv_final_output}\n")
+    print(f"\n Final results written to: {output_path}\n")
 
 
 if __name__ == "__main__":
